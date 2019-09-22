e706082c64634e8b66d1b75cce19764ce04927c4
wjc404 <52632443+wjc404@users.noreply.github.com>
2019-09-22 20:21:45 +0800

    Add files via upload

--- dgemm_tune.h
+++ dgemm_tune.h
@@ -2,14 +2,33 @@
 // The value of PREF_CYCLES_PACKED_A should be greater than the latency of L2 cache (in cycles).
 // The value of PREF_CYCLES_PACKED_B should be greater than the latency of L3 cache (in cycles).
 
+/* Parameters tuned on AMD R7-3700X; for Intel CPUs try multiplying GEMM_LOOP_TIMES_N by 2 */
 # define GEMM_UNROLL_N 6
 # define GEMM_LOOP_TIMES_N 16
 # define GEMM_LOOP_TIMES_K 6
-# define PREF_CYCLES_PACKED_A 24
-# define PREF_CYCLES_PACKED_B 96
+# define PREF_CYCLES_PACKED_A 18
+# define PREF_CYCLES_PACKED_B 72
 
-//# define GEMM_UNROLL_N 3
-//# define GEMM_LOOP_TIMES_N 32
-//# define GEMM_LOOP_TIMES_K 8
-//# define PREF_CYCLES_PACKED_A 12
-//# define PREF_CYCLES_PACKED_B 96
+/*
+# define GEMM_UNROLL_N 4
+# define GEMM_LOOP_TIMES_N 32
+# define GEMM_LOOP_TIMES_K 8
+# define PREF_CYCLES_PACKED_A 16
+# define PREF_CYCLES_PACKED_B 96
+*/
+
+/*
+# define GEMM_UNROLL_N 3
+# define GEMM_LOOP_TIMES_N 32
+# define GEMM_LOOP_TIMES_K 8
+# define PREF_CYCLES_PACKED_A 12
+# define PREF_CYCLES_PACKED_B 64
+*/
+
+/*
+# define GEMM_UNROLL_N 2
+# define GEMM_LOOP_TIMES_N 96
+# define GEMM_LOOP_TIMES_K 8
+# define PREF_CYCLES_PACKED_A 12
+# define PREF_CYCLES_PACKED_B 64
+*/
--- sgemm_tune.h
+++ sgemm_tune.h
@@ -2,14 +2,33 @@
 // The value of PREF_CYCLES_PACKED_A should be greater than the latency of L2 cache (in cycles).
 // The value of PREF_CYCLES_PACKED_B should be greater than the latency of L3 cache (in cycles).
 
+/*Parameters tuned on AMD R7-3700X*/
+# define GEMM_UNROLL_N 6
+# define GEMM_LOOP_TIMES_N 32
+# define GEMM_LOOP_TIMES_K 7
+# define PREF_CYCLES_PACKED_A 18
+# define PREF_CYCLES_PACKED_B 96
+
+/*
 # define GEMM_UNROLL_N 4
 # define GEMM_LOOP_TIMES_N 64
 # define GEMM_LOOP_TIMES_K 8
 # define PREF_CYCLES_PACKED_A 16
 # define PREF_CYCLES_PACKED_B 96
+*/
 
-//# define GEMM_UNROLL_N 2
-//# define GEMM_LOOP_TIMES_N 64
-//# define GEMM_LOOP_TIMES_K 8
-//# define PREF_CYCLES_PACKED_A 12
-//# define PREF_CYCLES_PACKED_B 96
+/*
+# define GEMM_UNROLL_N 3
+# define GEMM_LOOP_TIMES_N 64
+# define GEMM_LOOP_TIMES_K 8
+# define PREF_CYCLES_PACKED_A 12
+# define PREF_CYCLES_PACKED_B 64
+*/
+
+/*
+# define GEMM_UNROLL_N 2
+# define GEMM_LOOP_TIMES_N 128
+# define GEMM_LOOP_TIMES_K 8
+# define PREF_CYCLES_PACKED_A 12
+# define PREF_CYCLES_PACKED_B 64
+*/
