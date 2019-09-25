d0cc6e6c243f2bc14efa8ff63e62a42c21913f00
wjc404 <52632443+wjc404@users.noreply.github.com>
2019-09-25 19:36:37 +0800

    Update dgemm_tune.h

--- dgemm_tune.h
+++ dgemm_tune.h
@@ -2,15 +2,14 @@
 // The value of PREF_CYCLES_PACKED_A should be greater than the latency of L2 cache (in cycles).
 // The value of PREF_CYCLES_PACKED_B should be greater than the latency of L3 cache (in cycles).
 
-/* Parameters tuned on AMD R7-3700X */
+// Parameters tuned on AMD R7-3700X
 # define GEMM_UNROLL_N 6
 # define GEMM_LOOP_TIMES_N 16
 # define GEMM_LOOP_TIMES_K 6
 # define PREF_CYCLES_PACKED_A 18
 # define PREF_CYCLES_PACKED_B 72
 
-/* Parameters tuned on i9 9900K */
-/*
+/* //Parameters tuned on i9 9900K 
 # define GEMM_UNROLL_N 4
 # define GEMM_LOOP_TIMES_N 64
 # define GEMM_LOOP_TIMES_K 8
